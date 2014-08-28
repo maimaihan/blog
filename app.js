4f3a626edbd2218fb2c142d9da69ea0458d9767a
unknown <I301346@PVGN50858271A.apj.global.corp.sap>
2014-08-28 14:15:40 +0800

    realize post

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
 var express = require('express');
+var session = require('express-session'); 
+var cookieParser = require('cookie-parser');
 var path = require('path');
+//var MongoStore = require('connect-mongo')(express);
+var settings = require('./settings');
+var flash = require('connect-flash');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 
 var app = express();
+app.get('/home', function (req, res) {
+    res.send('Hello world!');
+    console.log(111);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(flash());
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser(settings.cookieSecret));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+ /* store: new MongoStore({
+    db: settings.db
+  })*/
+}));
+//app.use(express.methodOverride());
 
-app.use('/', routes);
-app.use('/users', users);
+
+//app.use('/', routes);
+//app.use('/users', users);
+routes(app);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
+});*/
 
 /// error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+/*if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -43,17 +64,23 @@ if (app.get('env') === 'development') {
             error: err
         });
     });
-}
+}*/
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
-});
+});*/
 
 
 module.exports = app;
+
+
+/*var server = app.listen(8080, function() {
+    console.log('Listening on port %d', server.address().port);
+});*/
+
